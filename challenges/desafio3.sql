cb20f4acb1242e80d741c2086c3ddd62c48a9a31
Levy Araujo <levy4raujo@gmail.com>
2023-04-19 16:20:47 -0300

    [Desafio 3] Concluído.
    Listando através de um INNER JOIN.

---
+++ challenges/desafio3.sql
@@ -0,0 +1,15 @@
+SELECT
+  usuario.nome AS pessoa_usuaria,
+  COUNT(historico.cancoes_id) AS musicas_ouvidas,
+  ROUND(SUM(cancoes.duracao_sec/60),2) AS total_minutos
+FROM
+  SpotifyClone.usuario AS usuario
+INNER JOIN
+  SpotifyClone.historico AS historico
+ON
+  usuario.pessoa_usuario_id = historico.pessoa_usuario_id
+INNER JOIN
+  SpotifyClone.cancoes AS cancoes
+ON
+  historico.cancoes_id = cancoes.cancoes_id
+GROUP BY nome ;
