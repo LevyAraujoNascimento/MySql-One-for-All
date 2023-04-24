f4545774d19243b401e7dd66dcef321c8f4525ba
Levy Araujo <levy4raujo@gmail.com>
2023-04-24 17:05:59 -0300

    [Desafio 10] Concluído.
    Criando uma tabela para musicas favoritas e inserindo dados.

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -1,13 +1,23 @@
--- Descomente e altere as linhas abaixo:
+CREATE TABLE SpotifyClone.favoritas(
+  pessoa_usuario_id INT NOT NULL,
+  cancoes_id INT NOT NULL,
+  PRIMARY KEY (pessoa_usuario_id, cancoes_id),
+  FOREIGN KEY (pessoa_usuario_id) REFERENCES usuario (pessoa_usuario_id),
+  FOREIGN KEY (cancoes_id) REFERENCES cancoes (cancoes_id)
+) engine = InnoDB;
 
--- CREATE TABLE SpotifyClone.tabela(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
-
--- INSERT INTO SpotifyClone.tabela (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
+INSERT INTO SpotifyClone.favoritas (pessoa_usuario_id, cancoes_id)
+VALUES
+  (1, 3),
+  (1, 6),
+  (1, 10),
+  (2, 4),
+  (3, 1),
+  (3, 3),
+  (4, 7),
+  (4, 4),
+  (5, 10),
+  (5, 2),
+  (6, 4),
+  (7, 7),
+  (8, 3);
